1dc362f5b9c5df22f040dbfceefaf26488260ac4
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-05 17:46:38 +0530

    Add files via upload

---
+++ while-3.py
@@ -0,0 +1,6 @@
+# while condition
+i=1               # i variable store 1
+while i<4:        # while condition for 1 is greater than 4
+    print(i)      # print i for while condition is true
+    i+=1          # execution for start to end execution formula
+    print("end")  # 1 value execution after this string execute
